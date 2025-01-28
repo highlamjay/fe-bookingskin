c49b375e690c61bf9a32775b1c0153751c293251
Enryu <you@example.com>
2025-01-28 14:45:56 +0700

    checkout hip

--- server/models/Post.js
+++ server/models/Post.js
@@ -17,6 +17,8 @@ const PostSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
+}, {
+    timestamps: true  // Adds createdAt and updatedAt automatically
 });
 
 module.exports = mongoose.model('Post', PostSchema);
